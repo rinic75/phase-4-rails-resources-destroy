5d8ebc738223f20ab6dfef037668bbca31ab8d41
Chang Shin <cshin7374@gmail.com>
2023-06-15 22:37:12 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  resources :birds, only: [:index, :show, :create, :update, :destory]
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
